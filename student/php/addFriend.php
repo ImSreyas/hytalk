df109df65675aa70c0f3656a06320eb609e30bad
ImSreyas <sreyassatheesh755@gmail.com>
2023-03-05 11:50:48 +0530

    main feed page is done

--- student/php/addFriend.php
+++ student/php/addFriend.php
@@ -3,10 +3,11 @@ session_start();
 include('../../php/db/db.php');
 if(isset($_SESSION['student_id'])){
     $id = $_GET['id'];
-    $user_type = $_GET['user_type'];
+    $friend_type = $_GET['user_type'];
     $user_id = $_GET['user_id'];
+    $user_type = $_GET['type'];
 
-    mysqli_query($conn, "insert into friends set user_id='$id', friend_id='$user_id', user_type='$user_type'");
-    header('location:../newsfeed.php');
+    mysqli_query($conn, "insert into friends set user_id='$id', friend_id='$user_id', friend_type='$friend_type', user_type='$user_type'");
+    header('location:../newsfeed-friends.php');
 }
 ?>
--- student/php/addFriendCancel.php
+++ student/php/addFriendCancel.php
@@ -3,10 +3,12 @@ session_start();
 include('../../php/db/db.php');
 if(isset($_SESSION['student_id'])){
     $id = $_GET['id'];
-    $user_type = $_GET['user_type'];
+    $friend_type = $_GET['user_type'];
+    $user_type = $_GET['type'];
     $user_id = $_GET['user_id'];
+    
 
-    mysqli_query($conn, "delete from friends where user_id='$id' && friend_id='$user_id' && user_type='$user_type'");
-    header('location:../newsfeed.php');
+    mysqli_query($conn, "delete from friends where user_id='$id' && friend_id='$user_id' && user_type='$user_type' && friend_type='$friend_type'");
+    header('location:../newsfeed-friends.php');
 }
 ?>
